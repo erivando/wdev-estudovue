86d57cef3c66961db357eab415b63d554d8c2694
erivando <erivandovasconcelos@hotmail.com>
2021-05-06 15:43:08 -0300

    Estudo Vuejs

---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer>
+
+    <a
+      class="socialLinks"
+      v-for="(social, index) in socialLinks"
+      :key="index"
+      :href="social.link"
+      target="_blank"
+    >
+      <img :src="social.icon" :alt="social.label" :title="social.label">
+    </a>
+
+  </footer>
+</template>
+
+<script>
+import api from '@/services/api.js';
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      socialLinks: [],
+    }
+  },
+  mounted() {
+    api.get('social-links.json').then(response => {
+      this.socialLinks = response.data;
+    })
+  }
+}
+</script>
+
+<style scoped>
+  footer {
+    width: 100%;
+    height: 60px;
+    background-color: var(--color-background-nav);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .socialLinks {
+    border: 2px solid var(--color-text-light);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    margin: 0 10px;
+  }
+
+  .socialLinks img {
+    width: 20px;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+    <div class="text-top">
+      Vídeos novos toda <b>quinta</b> às <b>20H</b>
+    </div>
+
+    <a href="https://youtube.com/wdevoficial" target="_blank">
+      <img src=".././assets/subscribe.svg" alt="Inscreva-se" id="subscribe">
+    </a>
+
+    <div class="text-botton">
+      youtube.com/wdevoficial
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+  main {
+    background-color: var(--color-background-home);
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text-top {
+    color: var(--color-text-light);
+    font-size: 22px;
+    margin-bottom: 20px;
+  }
+
+  #subscribe {
+    width: 340px;
+  }
+
+  .text-botton {
+    color: var(--color-text-light);
+    font-size: 18px;
+    margin-top: 20px;
+  }
+
+  @media(min-width: 700px) {
+    #subscribe {
+      width: 370px;
+    }
+  }
+</style>
